9c4385427a29d9702c2db4f634666a4c3caa2e86
NadiaJalalTamimi <nadia.tamimi74@gmail.com>
2021-08-17 21:16:33 +0300

    create server Relates #9

---
+++ server/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const { join } = require('path');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(join(__dirname, '..', 'public')));
+
+app.set('port', process.env.PORT || 6000);
+
+module.exports = app;
