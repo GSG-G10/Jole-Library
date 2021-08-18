5238c56551ce22ab9282e47a5f535e5f05a45801
Raghad-Mezied <rmezied3@gmail.com>
2021-08-18 20:58:46 +0300

    edite connection

--- server/database/config/conection.js
+++ server/database/config/conection.js
@@ -1,29 +1,29 @@
 require('env2')('./config.env');
 const { Pool } = require('pg');
 
-// let dbUrl = '';
-// const {
-//   NODE_ENV, DB_DEV_URL, DB_URL, TEST_DB_URL,
-// } = process.env;
+let dbUrl = '';
+const {
+  NODE_ENV, DB_DEV_URL, DB_URL, TEST_DB_URL,
+} = process.env;
 
-// switch (NODE_ENV) {
-//   case 'production':
-//     dbUrl = DB_URL;
-//     break;
-//   case 'development':
-//     dbUrl = DB_DEV_URL;
-//     break;
-//   case 'test':
-//     dbUrl = TEST_DB_URL;
-//     break;
-//   default:
-//     throw new Error('NO DATABASE to show!');
-// }
+switch (NODE_ENV) {
+  case 'production':
+    dbUrl = DB_URL;
+    break;
+  case 'development':
+    dbUrl = DB_DEV_URL;
+    break;
+  case 'test':
+    dbUrl = TEST_DB_URL;
+    break;
+  default:
+    throw new Error('NO DATABASE to show!');
+}
 
 const options = {
-//   connectionString: dbUrl,
+  connectionString: dbUrl,
   connectionString: process.env.DB_DEV_URL,
-  ssl: false,
+  ssl: true,
 };
 
 module.exports = new Pool(options);
