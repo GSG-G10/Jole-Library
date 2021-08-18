5b05357bfc7d6899f785a0f22e77185c96d96003
NadiaJalalTamimi <nadia.tamimi74@gmail.com>
2021-08-18 10:52:43 +0300

    add router to app

--- server/app.js
+++ server/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const { join } = require('path');
+const router = require("./controllers")
+
 
 const app = express();
 
@@ -8,5 +10,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(join(__dirname, '..', 'public')));
 
 app.set('port', process.env.PORT || 2000);
+app.use(router);
 
 module.exports = app;
